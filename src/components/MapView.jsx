0c9c8731519b7aa00cc8458d0480d5f2113b72b8
aleyna <aleynatmacaa5@gmail.com>
2026-07-23 17:07:45 +0300

    add dynamic markers

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -2,21 +2,32 @@ import { MapContainer } from "react-leaflet";
 import { TileLayer } from "react-leaflet";
 import { Popup } from "react-leaflet";
 import { Marker } from "react-leaflet";
+const coffeeShops = [
+  {name:"starbucks",
+  latitude:39.903324,
+longitude:32.860427,
+  },
+  {name:"Arabica",
+latitude:39.907067,
+longitude:32.860157,
+
+  },
+];
 function MapView () {
     return( <MapContainer zoom={13}
     center={[39.9334, 32.8597]}
     style={{ height: "600px", width: "100%" }} >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-  
-
-<Marker position={[39.9334, 32.8597]}
-position={[39.9048, 32.8573]}> 
-    <Popup> "it's center here"
-
-    </Popup>
-    </Marker>
-   
-    </MapContainer>
+  {coffeeShops.map((coffeeShop) => {
+    return(
+     <Marker
+        position={[coffeeShop.latitude,coffeeShop.longitude]}>
+            <Popup>
+                {coffeeShop.name}
+            </Popup>
+    </Marker>)
+})}
+</MapContainer>
 
     );
 }
